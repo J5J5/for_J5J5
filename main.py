84e54ee67a0ef7625b2668ad2c04a78684913baf
J5J5 <107280795+J5J5@users.noreply.github.com>
2022-06-14 02:49:38 +0300

    Update main.py

--- main.py
+++ main.py
@@ -292,7 +292,7 @@ def masg(call):
 				mas = types.InlineKeyboardMarkup(row_width=2)
 				A = types.InlineKeyboardButton(f'GOOD : {ok}',callback_data="1x")
 				E = types.InlineKeyboardButton(f'EROR : {cp}', callback_data="1x")
-				B = types.InlineKeyboardButton(f'{username}', callback_data="1x")
+				B = types.InlineKeyboardButton(f'{c}', callback_data="1x")
 				R = types.InlineKeyboardButton(f'{sk}', callback_data="1x")
 				M = types.InlineKeyboardButton('DEV', url='https://t.me/K_8_U')
 				mas.add(A,E,B,R,M)
