14d48cff688a736b8c52db0bf95df090b96c8dce
J5J5 <107280795+J5J5@users.noreply.github.com>
2022-06-13 15:07:46 +0300

    Update main.py

--- main.py
+++ main.py
@@ -16,20 +16,15 @@ logger.setLevel(logging.DEBUG)
 
 @bot.message_handler(commands=['start'])
 def boten(message):
-	
-    
-    
     mas = types.InlineKeyboardMarkup(row_width=2)
     A = types.InlineKeyboardButton(text ="(KKKK4)", callback_data="F1")
-    E = types.InlineKeyboardButton(text ="(FF8FF)", callback_data="F2")
-    K = types.InlineKeyboardButton(text ="(K_8_U)", callback_data="F3")
+    K = types.InlineKeyboardButton(text ="(يوزرات ثلاثيه)", callback_data="F3")
     F = types.InlineKeyboardButton(text ="(UUU8UU)", callback_data="F7")
-	G = types.InlineKeyboardButton(text ="(يوزرات بوتات)", callback_data="F8")
+    G = types.InlineKeyboardButton(text ="(يوزرات بوتات)", callback_data="F8")
     M = types.InlineKeyboardButton('DEV', url='https://t.me/K_8_U')
-	mas.add(G)
-    mas.add(A,E,K,F)
+    mas.add(G,K)
+    mas.add(A,F)
     mas.add(M)
-    
     bot.send_message(message.chat.id, f"- أهلاً بكً  !\n\n- بوت تشكير يوزرات تلجرام 🧑‍💻\n\n♻️ لوحة التحكم الخاصه بك ♨️",reply_markup=mas)
     
     
@@ -106,28 +101,151 @@ def masg(call):
 			
 	elif call.data =="F8":
 		e = types.InlineKeyboardMarkup(row_width=2)
-		f = types.InlineKeyboardButton(text="c8bot",callback_data="b1")
-		c = types.InlineKeyboardButton(text="vd2bot",callback_data="b2")
-		x = types.InlineKeyboardButton(text="iovbot",callback_data="b3")
-		z = types.InlineKeyboardButton(text="wdv4bot",callback_data="b4")
-		w = types.InlineKeyboardButton(text="vhdkbot",callback_data="b5")
-		v = types.InlineKeyboardButton(text="c3ebot",callback_data="b6")
+		f = types.InlineKeyboardButton(text="(c8bot)",callback_data="b1")
+		c = types.InlineKeyboardButton(text="(vd2bot)",callback_data="b2")
+		z = types.InlineKeyboardButton(text="(wdv4bot)",callback_data="b4")
 		bc = types.InlineKeyboardButton(text="رجوع",callback_data="bckkk")
-		e.add(f,c,x,z,w,v)
-		e.add(bk)
+		e.add(f,c,z)
+		e.add(bc)
 		bot.edit_message_text(chat_id=call.message.chat.id,message_id=call.message.message_id,"اختر من القائمه بالاسفل .",reply_markup=e)
 	elif call.data =="bckkk":
 		mas = types.InlineKeyboardMarkup(row_width=2)
 		A = types.InlineKeyboardButton(text ="(KKKK4)", callback_data="F1")
-		E = types.InlineKeyboardButton(text ="(FF8FF)", callback_data="F2")	
-		K = types.InlineKeyboardButton(text ="(Q_8_P)", callback_data="F3")	
+		K = types.InlineKeyboardButton(text ="(يوزرات ثلاثيه)", callback_data="F3")	
 		F = types.InlineKeyboardButton(text ="(UUU4UU)", callback_data="F7")
 		G = types.InlineKeyboardButton(text ="(يوزرات بوتات)", callback_data="F8")
 		M = types.InlineKeyboardButton('DEV', url='https://t.me/K_8_U')
-		mas.add(G)
-		mas.add(A,E,K,F)
+		mas.add(G,K)
+		mas.add(A,F)
 		mas.add(M)
 		bot.edit_message_text(chat_id=call.message.chat.id,message_id=call.message.message_id, text="- أهلاً بكً عزيزي المستخدم \n\n- بوت تشكير يوزرات تلجرام 🧑‍💻\n\n♻️ لوحة التحكم الخاصه بك ♨️",reply_markup=mas)
+		
+		elif call.data =="b4":
+			xu = "MNBVCXZLKJHGFDSAPOIUYTREWQ"
+			xn = "1234567890"
+			xa = "MNBVCXZLKJHGFDSAPOIUYTREWQ"
+			ok=0
+			cp=0
+			sk=0
+			while True:
+				us = str(''.join(random.choice(xu)for i in range(1)))
+				u2s = str(''.join(random.choice(xu)for i in range(1)))
+				u1s = str(''.join(random.choice(xu)for i in range(1)))
+				un = str(''.join(random.choice(xn)for i in range(1)))
+				u1n = str(''.join(random.choice(xn)for i in range(1)))
+				ua = str(''.join(random.choice(xa)for i in range(1)))
+				u1= str(us)+str(u1s)+str(u2s)+str(un)+"bot"
+				u2 = str(us)+str(un)+str(u2s)+str(u1s)+"bot"
+				u3 = str(us)+str(u1s)+str(un)+str(u2s)+"bot"
+				g = [u1,u2,u3]
+				x = random.choice(g)
+				url = "https://t.me/"+x
+				headers = {
+            "User-Agent": generate_user_agent(),
+            "Accept": "text/html,application/xhtml+xml,application/xml;q=0.9,image/webp,image/apng,*/*;q=0.8",
+            "Accept-Encoding": "gzip, deflate, br",
+            "Accept-Language" : "ar-EG,ar;q=0.9,en-US;q=0.8,en;q=0.7"}
+				response = requests.get(url, headers=headers)
+				if response.text.find('If you have <strong>Telegram</strong>, you can contact <a class="tgme_username_link"')>=0:
+					ok+=1
+					sk+=1
+					bot.send_message(call.message.chat.id,f"‹ يوزرات تلي متاحه ✓\n────── • ✧✧ • ──────\n‹ صدتلك يوزر : @{username}\n────── • ✧✧ • ──────\n• مطور البوت @K_8_U")
+				else:
+					cp+=1
+					sk+=1
+					mas = types.InlineKeyboardMarkup(row_width=2)
+					A = types.InlineKeyboardButton(f'GOOD : {ok}',callback_data="1x")
+					E = types.InlineKeyboardButton(f'EROR : {cp}', callback_data="1x")
+					B = types.InlineKeyboardButton(f'{username}', callback_data="1x")
+					R = types.InlineKeyboardButton(f'{sk}', callback_data="1x")
+					M = types.InlineKeyboardButton('DEV', url='https://t.me/K_8_U')
+					mas.add(A,E,B,R,M)
+					bot.edit_message_text(chat_id=call.message.chat.id,message_id=call.message.message_id,text="ok start",reply_markup=mas)
+	
+		
+		elif call.data =="b2":
+			xu = "MNBVCXZLKJHGFDSAPOIUYTREWQ"
+			xn = "1234567890"
+			xa = "MNBVCXZLKJHGFDSAPOIUYTREWQ"
+			ok=0
+			cp=0
+			sk=0
+			while True:
+				us = str(''.join(random.choice(xu)for i in range(1)))
+				u2s = str(''.join(random.choice(xu)for i in range(1)))
+				u1s = str(''.join(random.choice(xu)for i in range(1)))
+				un = str(''.join(random.choice(xn)for i in range(1)))
+				u1n = str(''.join(random.choice(xn)for i in range(1)))
+				ua = str(''.join(random.choice(xa)for i in range(1)))
+				u1= str(us)+str(u1s)+str(un)+"bot"
+				u2 = str(us)+str(u1s)+str(u2s)+"bot"
+				u3 = str(us)+str(un)+str(u1s)+"bot"
+				g = [u1,u2,u3]
+				x = random.choice(g)
+				url = "https://t.me/"+x
+				headers = {
+            "User-Agent": generate_user_agent(),
+            "Accept": "text/html,application/xhtml+xml,application/xml;q=0.9,image/webp,image/apng,*/*;q=0.8",
+            "Accept-Encoding": "gzip, deflate, br",
+            "Accept-Language" : "ar-EG,ar;q=0.9,en-US;q=0.8,en;q=0.7"}
+				response = requests.get(url, headers=headers)
+				if response.text.find('If you have <strong>Telegram</strong>, you can contact <a class="tgme_username_link"')>=0:
+					ok+=1
+					sk+=1
+					bot.send_message(call.message.chat.id,f"‹ يوزرات تلي متاحه ✓\n────── • ✧✧ • ──────\n‹ صدتلك يوزر : @{username}\n────── • ✧✧ • ──────\n• مطور البوت @K_8_U")
+				else:
+					cp+=1
+					sk+=1
+					mas = types.InlineKeyboardMarkup(row_width=2)
+					A = types.InlineKeyboardButton(f'GOOD : {ok}',callback_data="1x")
+					E = types.InlineKeyboardButton(f'EROR : {cp}', callback_data="1x")
+					B = types.InlineKeyboardButton(f'{username}', callback_data="1x")
+					R = types.InlineKeyboardButton(f'{sk}', callback_data="1x")
+					M = types.InlineKeyboardButton('DEV', url='https://t.me/K_8_U')
+					mas.add(A,E,B,R,M)
+					bot.edit_message_text(chat_id=call.message.chat.id,message_id=call.message.message_id,text="ok start",reply_markup=mas)
+	
+		
+		elif call.data =="b1":
+			xu = "MNBVCXZLKJHGFDSAPOIUYTREWQ"
+			xn = "1234567890"
+			xa = "MNBVCXZLKJHGFDSAPOIUYTREWQ"
+			ok=0
+			cp=0
+			sk=0
+			while True:
+				us = str(''.join(random.choice(xu)for i in range(1)))
+				u2s = str(''.join(random.choice(xu)for i in range(1)))
+				u1s = str(''.join(random.choice(xu)for i in range(1)))
+				un = str(''.join(random.choice(xn)for i in range(1)))
+				u1n = str(''.join(random.choice(xn)for i in range(1)))
+				ua = str(''.join(random.choice(xa)for i in range(1)))
+				u1= str(us)+str(un)+"bot"
+				u2 = str(us)+str(u1s)+"bot"
+				f = [u1,u2]
+				v = random.choice(f)
+				url = "https://t.me/"+v
+				headers = {
+            "User-Agent": generate_user_agent(),
+            "Accept": "text/html,application/xhtml+xml,application/xml;q=0.9,image/webp,image/apng,*/*;q=0.8",
+            "Accept-Encoding": "gzip, deflate, br",
+            "Accept-Language" : "ar-EG,ar;q=0.9,en-US;q=0.8,en;q=0.7"}
+				response = requests.get(url, headers=headers)
+				if response.text.find('If you have <strong>Telegram</strong>, you can contact <a class="tgme_username_link"')>=0:
+					ok+=1
+					sk+=1
+					bot.send_message(call.message.chat.id,f"‹ يوزرات تلي متاحه ✓\n────── • ✧✧ • ──────\n‹ صدتلك يوزر : @{username}\n────── • ✧✧ • ──────\n• مطور البوت @K_8_U")
+				else:
+					cp+=1
+					sk+=1
+					mas = types.InlineKeyboardMarkup(row_width=2)
+					A = types.InlineKeyboardButton(f'GOOD : {ok}',callback_data="1x")
+					E = types.InlineKeyboardButton(f'EROR : {cp}', callback_data="1x")
+					B = types.InlineKeyboardButton(f'{username}', callback_data="1x")
+					R = types.InlineKeyboardButton(f'{sk}', callback_data="1x")
+					M = types.InlineKeyboardButton('DEV', url='https://t.me/K_8_U')
+					mas.add(A,E,B,R,M)
+					bot.edit_message_text(chat_id=call.message.chat.id,message_id=call.message.message_id,text="ok start",reply_markup=mas)
 	elif call.data =="F3":
 		
 		xu = "MNBVCXZLKJHGFDSAPOIUYTREWQ"
@@ -138,19 +256,18 @@ def masg(call):
 		sk=0
 		while True:
 			us = str(''.join(random.choice(xu)for i in range(1)))
+            u2s = str(''.join(random.choice(xu)for i in range(1)))
+            u1s = str(''.join(random.choice(xu)for i in range(1)))
 			un = str(''.join(random.choice(xn)for i in range(1)))
+            u1n = str(''.join(random.choice(xn)for i in range(1)))
 			ua = str(''.join(random.choice(xa)for i in range(1)))
-			u1= str(us)+"_"+str(un)+"_"+str(us)
-			u1= str(us)+"_"+str(un)+"_"+str(us)
-			u1= str(us)+"_"+str(un)+"_"+str(us)
-			u1= str(us)+"_"+str(un)+"_"+str(us)
-			u1= str(us)+"_"+str(un)+"_"+str(us)
-			u1= str(us)+"_"+str(un)+"_"+str(us)
-			u1= str(us)+"_"+str(un)+"_"+str(us)
-			u1= str(us)+"_"+str(un)+"_"+str(us)
-			u1= str(us)+"_"+str(un)+"_"+str(us)
-			S = []
-			url = "https://t.me/"+S
+			u1= str(us)+"_"+str(un)+"_"+str(u1s)
+			u2= str(us)+"_"+str(u1s)+"_"+str(un)
+			u3= str(us)+"_"+str(u1n)+"_"+str(un)
+			u4= str(us)+"_"+str(u1s)+"_"+str(u2s)
+			S = [u1,u2,u3,u4]
+            c = random.choice(S)
+			url = "https://t.me/"+c
 			headers = {
             "User-Agent": generate_user_agent(),
             "Accept": "text/html,application/xhtml+xml,application/xml;q=0.9,image/webp,image/apng,*/*;q=0.8",
