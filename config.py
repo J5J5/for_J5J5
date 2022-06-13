f722359d4c9f073ee75d54c57f6a2f2a43b0597d
J5J5 <107280795+J5J5@users.noreply.github.com>
2022-06-13 23:15:31 +0300

    Update config.py

--- config.py
+++ config.py
@@ -1,6 +1,6 @@
 import os
 
 BOT_TOKEN = os.environ.get("BOT_TOKEN", "")
-sudo = os.environ.get("SUDO", "")
+SUDO = os.environ.get("SUDO", "")
 HEROKU = os.environ.get("HEROKU", "APP-NAME")
 APP_URL = "https://"+ HEROKU +".herokuapp.com/" + BOT_TOKEN
